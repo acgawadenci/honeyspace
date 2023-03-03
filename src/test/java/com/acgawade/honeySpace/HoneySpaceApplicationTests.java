fc7c8740f3824f2c85766cc25828765bcec4bdc8
Achyut Gawade <gawade.achyut96@gmail.com>
2023-03-03 11:06:04 +0000

    Initial commit with basic project dependencies and skeleton

---
+++ src/test/java/com/acgawade/honeySpace/HoneySpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acgawade.honeySpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneySpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

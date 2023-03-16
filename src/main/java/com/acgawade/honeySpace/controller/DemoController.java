73e04fc5624a95a4dab456dda8eb39c6fd7f641b
Achyut Gawade <gawade.achyut96@gmail.com>
2023-03-16 14:40:37 +0000

    Demo controller updated

--- src/main/java/com/acgawade/honeySpace/controller/DemoController.java
+++ src/main/java/com/acgawade/honeySpace/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.acgawade.honeySpace.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class DemoController {
     @GetMapping("/")
     public String demoResponse() {
         return "This is my demo Controller, Hello User !!!";
+    }
+
+    @GetMapping("/{userName}")
+    public String demoResp(@PathVariable String userName) {
+        return "This is also my demo Controller, Hello User !!!";
     }
 }
